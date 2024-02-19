f8cf07102a336d699c28553842110145921975b9
JIYOUNG <0803jiyoung99@gmail.com>
2024-02-19 10:30:41 +0900

    Feat: (3) Router 셋업

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Detail from "pages/Detail";
 import Home from "pages/Home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Profile from "pages/Profile";
+import Login from "pages/Login";
 
 function Router() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isLogin ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
