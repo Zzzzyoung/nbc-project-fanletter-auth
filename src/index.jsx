9606a68f88eb0033bb95afa90de0251b8d2b4cb8
JIYOUNG <0803jiyoung99@gmail.com>
2024-03-09 00:52:48 +0900

    Chore: react-query 기본 세팅

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,19 @@ import store from "./redux/config/configStore";
 import GlobalStyle from "style/GlobalStyle";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-    <GlobalStyle />
-    <ToastContainer />
-  </Provider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+        <GlobalStyle />
+        <ToastContainer />
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
